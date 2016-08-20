e60c8eaaf47a867442a485caaf6db8555075966d
Margaret Liu <margs.liu@gmail.com>
2016-08-20 18:18:18 -0500

    create HTML for forms, and welcome page

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for header and footer
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
+
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        edit_header = "<h1>Sign up <u>now</u> to have onions delivered to you <em>daily!</em></h1>"
+
+        form = """
+        <form action="/welcome" method="post">
+            <label>Username</label>
+                <input type="text" name="username" required/>
+            <br>
+            <label>Password</label>
+                <input type="password" name="password1" required/>
+            <br>
+            <label>Verify Password</label>
+                <input type="password" name="password2" required/>
+            <br>
+            <label>Email (optional)</label>
+                <input type="email" name="useremail"/>
+            <br>
+            <input type="submit" value="Submit"/>
+        </form>
+        """
+
+        main_content = edit_header + form
+        response = page_header + main_content + page_footer
+        self.response.write(response)
+
+class Welcome(webapp2.RequestHandler):
+    def post(self):
+
+        username = self.request.get("username")
+
+        welcome_message = "<h1>Welcome, {0}! You're gonna have SO many onions now.</h1>".format(username)
+
+        main_content = welcome_message
+
+        response = page_header + main_content + page_footer
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/welcome', Welcome)
 ], debug=True)
