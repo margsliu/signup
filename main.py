6b1873f33fb56311528aa1eda3c02efc0689e96d
Margaret Liu <margs.liu@gmail.com>
2016-08-25 19:44:17 -0500

    implement username, password character validations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webapp2
 import cgi
+import re
 
 # html boilerplate for header and footer
 page_header = """
@@ -21,38 +22,32 @@ page_footer = """
 </html>
 """
 
-def validatepass(pw1,pw2):
-    """compares and verifies that user input passwords are same"""
-    if pw1 != pw2:
-        return False
-    else:
-        return True
-
 class Index(webapp2.RequestHandler):
     def get(self):
         edit_header = "<h1>Sign up <u>now</u> to have onions delivered to you <em>daily!</em></h1>"
 
-        error = self.request.get('error')
+        usernameerror = self.request.get('uerror')
+        passworderror = self.request.get('perror')
         username = self.request.get('username')
         useremail = self.request.get('useremail')
 
         form = """
         <form action="/welcome" method="post">
             <label>Username</label>
-                <input type="text" name="username" value="{uname}" required/>
+                <input type="text" name="username" value="{uname}" required/> <font style="color:red" pattern="">{uerror}</font>
             <br>
             <label>Password</label>
                 <input type="password" name="password1" required/>
             <br>
             <label>Verify Password</label>
-                <input type="password" name="password2" required/> <font style="color:red">{perror}</font>
+                <input type="password" name="password2" required/> <font style="color:red" pattern="">{perror}</font>
             <br>
             <label>Email (optional)</label>
                 <input type="email" name="useremail" value="{uemail}"/>
             <br>
             <input type="submit" value="Submit"/>
         </form>
-        """.format(uname=username, perror=error, uemail=useremail)
+        """.format(uname=username, uerror=usernameerror, perror=passworderror, uemail=useremail)
 
         main_content = edit_header + form
         response = page_header + main_content + page_footer
@@ -64,13 +59,31 @@ class Welcome(webapp2.RequestHandler):
 
     def post(self):
 
-        if validatepass(self.request.get("password1"),self.request.get("password2")) == False:
-            error = "Passwords don't match."
-            username = self.request.get('username')
-            useremail = self.request.get('useremail')
-            self.redirect('/?error={}&username={}&useremail={}'.format(cgi.escape(error, quote=True),username,useremail))
+        uerror = ""
+        perror = ""
+        username = self.request.get('username')
+        password1 = self.request.get("password1")
+        password2 = self.request.get('password2')
+        useremail = self.request.get('useremail')
 
-        username = self.request.get("username")
+        user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        password_re = re.compile(r"^.{3,20}$")
+
+        # check if passwords match
+        if not password1 == password2:
+            perror = "Passwords don't match."
+            self.redirect('/?perror={}&username={}&useremail={}'.format(cgi.escape(perror, quote=True),username,useremail))
+
+        # check if password is valid
+        if password_re.match(password1) == None:
+            perror = "Invalid password, try again."
+            self.redirect('/?perror={}&username={}&useremail={}'.format(cgi.escape(perror, quote=True),username,useremail))
+
+        # check if username is valid
+        if user_re.match(username) == None:
+            uerror = "Invalid username, try again."
+            self.redirect('/?uerror={}&useremail={}'.format(cgi.escape(uerror, quote=True),useremail))
+
 
         welcome_message = "<h1>Welcome, {0}! You're gonna have SO many onions now.</h1>".format(username)
 
